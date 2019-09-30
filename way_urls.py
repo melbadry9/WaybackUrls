9f69f45a565ca9ce05412c60d9931a2eff7fe722
Mohamed Elbadry <melbadry9@github.com>
2019-09-30 06:38:59 +0200

    add support to pipe

--- way_urls.py
+++ way_urls.py
@@ -5,17 +5,25 @@ import hashlib
 import requests
 from urllib.parse import unquote
 
-#read domain string
-domain = sys.argv[1]
 
-#create name hash
-file_time = time.time()
-file_name = hashlib.md5(domain.encode("utf-8")).hexdigest() + "-" + str(file_time).split(".")[0] + ".txt"
+def get_data(domain):
+    file_name = domain.replace("*.","") + ".txt"
+    url = "http://web.archive.org/cdx/search/cdx?url={}/*&output=text&fl=original&collapse=urlkey"
+    res = requests.get(url.format(domain), stream=True, timeout=60)
+    with open(file_name, "a", encoding="utf-8") as file:
+        file.write(unquote(res.text))
 
-#url
-url = "http://web.archive.org/cdx/search/cdx?url={}/*&output=text&fl=original&collapse=urlkey"
+def main():
+    try:
+        domain = [sys.argv[1]]
+    except IndexError:
+        domain = sys.stdin
+        if domain.isatty(): print("[x] Domain is missing"); exit()
 
-res = requests.get(url.format(domain), stream=True)
+    for dom in domain:
+        dom = dom.rstrip("\n")
+        print( "[i] Getting urls for: %s" % dom )
+        get_data(dom)
 
-with open(file_name, "a", encoding="utf-8") as file:
-    file.write(unquote(res.text))
+if __name__ == "__main__":
+    main()
